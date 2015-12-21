f9cb088dbb93a6fe82e321e67471e448da72b328
Gregory Haskins <gregory.haskins@gmail.com>
2015-12-21 14:59:52 -0500

    Add hello-world test case
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- main_test.go
+++ main_test.go
@@ -2,13 +2,24 @@ package jvmbind_test
 
 import (
 	"testing"
+	"strings"
 	"github.com/ghaskins/jvmbind"
 )
 
 func TestLaunch(t *testing.T) {
-	instance, err := jvmbind.Launch("./test/helloworld.jar")
+	instance, err := jvmbind.Launch("./test/hello-world.jar")
 	if err != nil {
 		t.Error(err)
+	}
+
+	_msg := make([]byte, 256)
+
+	stdout := instance.Stdout
+	stdout.Read(_msg)
+	msg := string(_msg)
+
+	if strings.Contains(msg, "Hello World!") == false {
+		t.Errorf("Unexpected response: %s", msg)
 	}
 
 	instance.Wait()
