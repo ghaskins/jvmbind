3fa0a6575226ee9447c0df067d710516c1cd2205
Gregory Haskins <gregory.haskins@gmail.com>
2015-12-22 18:21:56 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- test/echo-server/src/echo_server/core.clj
+++ test/echo-server/src/echo_server/core.clj
@@ -6,24 +6,26 @@
   (.getInt (java.nio.ByteBuffer/wrap data)))
 
 (defn rawrecv [nr]
-  (let [buf (byte-array nr)
-        cr (ConsoleReader.)]
-    (for [i (range nr)] (aset-byte buf i (.readCharacter cr)))
+  (let [buf (byte-array nr)]
+    (for [i (range nr)] (aset-byte buf i (.read *in*)))
     buf))
 
 (defn recv []
   (let [len (uint32 (rawrecv 4))]
+    (println "recv" len "bytes")
     (rawrecv len)))
 
 (defn rawsend [data]
   (dorun (map #(.append *out* (char %)) data)))
 
 (defn send [payload]
-  (let [header (byte-array 4)]
-    (.putInt (java.nio.ByteBuffer/wrap header) (count payload))
-    (rawsend header)
-    (rawsend payload)
-    (.flush *out*)))
+  (when-let [len (count payload)]
+    (let [header (byte-array 4)]
+      (println "sending" len "bytes")
+      (.putInt (java.nio.ByteBuffer/wrap header) len)
+      (rawsend header)
+      (rawsend payload)
+      (.flush *out*))))
 
 (defn -main
   "loops forever echoing stdin packets to stdout"
