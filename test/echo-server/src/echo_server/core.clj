a7a85de820aac979babace247556b85898f4c378
Gregory Haskins <gregory.haskins@gmail.com>
2015-12-22 17:55:50 -0500

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- test/echo-server/src/echo_server/core.clj
+++ test/echo-server/src/echo_server/core.clj
@@ -1,7 +1,33 @@
 (ns echo-server.core
+  (:import [jline.console ConsoleReader])
   (:gen-class))
 
+(defn uint32 [data]
+  (.getInt (java.nio.ByteBuffer/wrap data)))
+
+(defn rawrecv [nr]
+  (let [buf (byte-array nr)
+        cr (ConsoleReader.)]
+    (for [i (range nr)] (aset-byte buf i (.readCharacter cr)))
+    buf))
+
+(defn recv []
+  (let [len (uint32 (rawrecv 4))]
+    (rawrecv len)))
+
+(defn rawsend [data]
+  (dorun (map #(.append *out* (char %)) data)))
+
+(defn send [payload]
+  (let [header (byte-array 4)]
+    (.putInt (java.nio.ByteBuffer/wrap header) (count payload))
+    (rawsend header)
+    (rawsend payload)
+    (.flush *out*)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "loops forever echoing stdin packets to stdout"
   [& args]
-  (println "Hello, World!"))
+  (send (recv)))
+
+  
