de7a93cc6b742c7ca4ee09ff469966eb0de58076
Gregory Haskins <gregory.haskins@gmail.com>
2015-12-22 21:43:28 -0500

    Generalize the echo-server's stream interface
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- test/echo-server/src/echo_server/core.clj
+++ test/echo-server/src/echo_server/core.clj
@@ -5,31 +5,31 @@
 (defn uint32 [data]
   (.getInt (java.nio.ByteBuffer/wrap data)))
 
-(defn rawrecv [nr]
+(defn rawrecv [nr stream]
   (let [buf (byte-array nr)]
-    (for [i (range nr)] (aset-byte buf i (.read *in*)))
+    (for [i (range nr)] (aset-byte buf i (.read stream)))
     buf))
 
-(defn recv []
-  (let [len (uint32 (rawrecv 4))]
+(defn recv [stream]
+  (let [len (uint32 (rawrecv 4 stream))]
     (println "recv" len "bytes")
-    (rawrecv len)))
+    (rawrecv len stream)))
 
-(defn rawsend [data]
-  (dorun (map #(.append *out* (char %)) data)))
+(defn rawsend [stream data]
+  (dorun (map #(.append stream (char %)) data)))
 
-(defn send [payload]
+(defn send [stream payload]
   (when-let [len (count payload)]
     (let [header (byte-array 4)]
       (println "sending" len "bytes")
       (.putInt (java.nio.ByteBuffer/wrap header) len)
-      (rawsend header)
-      (rawsend payload)
-      (.flush *out*))))
+      (rawsend stream header)
+      (rawsend stream payload)
+      (.flush stream))))
 
 (defn -main
   "loops forever echoing stdin packets to stdout"
   [& args]
-  (send (recv)))
+  (send *out* (recv *in*)))
 
   
